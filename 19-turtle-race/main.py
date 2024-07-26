242f6bad10672a6da243b2ad0210a31a92daea37
Mateus-Teixeira-WOW <mateus-teixeira-eu@gmail.com>
2024-07-26 07:14:31 -0300

    day-19 complete

--- 19-turtle-race/main.py
+++ 19-turtle-race/main.py
@@ -2,11 +2,36 @@ from turtle import *
 from random import *
 
 screen = Screen()
+screen.setup(500, 400)
+user_bet = screen.textinput(title="Make your bet!", prompt="Wich turtle is the fastest? Chose a color between: red, "
+                                                           "green, yellow, blue, orange and purple")
+positions = [-70, -40, -10, 20, 50, 80]
+colors = ["red", "green", "yellow", "blue", "orange", "purple"]
+all_turtles = []
 
-positions = []
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=positions[turtle_index])
+    all_turtles.append(new_turtle)
 
+is_race_on = False
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner = turtle.color()
+            if user_bet == winner:
+                print(f"You won! The winner is {winner}")
+            else:
+                print(f"You lost. The winner is {winner}")
+        else:
+            turtle.forward(randint(5, 20))
 
 
 screen.exitonclick()
--- README.md
+++
@@ -1,2 +0,0 @@
-# 100-days-of-python
-I chalange myself to complete 100 days of of 100 Python projects from the Angela's Yu Udemy course: Master Python by building 100 projects in 100 days. Learn data science, automation, build websites, games and apps!
